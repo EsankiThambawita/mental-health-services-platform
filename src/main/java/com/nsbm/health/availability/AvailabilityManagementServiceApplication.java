9866888d23157f1d0ca25c7e7c2886af9c640e45
Oneli Wickramaratne <onithesanya@gmail.com>
2026-02-01 19:17:52 +0530

    Initial commit of availability-management-service
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/nsbm/health/availability/AvailabilityManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsbm.health.availability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvailabilityManagementServiceApplication.class, args);
+    }
+
+}
