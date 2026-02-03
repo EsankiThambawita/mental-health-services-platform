6c236baae4e65c784c058541b4f6ead20ceb7aa9
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-03 09:06:55 +0530

    initial project setup

---
+++ backend/services/session-communication-service/src/test/java/com/mentalhealth/sessioncommunicationservice/SessionCommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentalhealth.sessioncommunicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionCommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
