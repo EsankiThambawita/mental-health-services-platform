32108cf8af3e0ce672f81dc1c06d00587f1c8307
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-05 09:57:20 +0530

    Initial classes and package creation. Created packages & classes for websocket setup, db access, MVC, data transfer, error handling

--- backend/services/session-communication-service/src/main/java/com/mentalhealth/chat/SessionCommunicationServiceApplication.java
+++ backend/services/session-communication-service/src/main/java/com/mentalhealth/chat/SessionCommunicationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mentalhealth.sessioncommunicationservice;
+package com.mentalhealth.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/services/session-communication-service/src/main/java/com/mentalhealth/chat/service/ChatMessageService.java
@@ -0,0 +1,5 @@
+package com.mentalhealth.chat.service;
+
+public class ChatMessageService {
+
+}
