4c7061f55d7a945d706b9e5b89f0dbca24eaf4d7
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-06 13:15:11 +0530

    springboot project setup and initial testing

--- backend/services/session-communication-service/src/test/java/com/mentalhealth/sessioncommunicationservice/SessionCommunicationServiceApplicationTests.java
+++ backend/services/session-communication-service/src/test/java/com/mentalhealth/sessioncommunicationservice/SessionCommunicationServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.mentalhealth.sessioncommunicationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.mentalhealth.chat.SessionCommunicationServiceApplication;
+
+@SpringBootTest(classes = SessionCommunicationServiceApplication.class)
 class SessionCommunicationServiceApplicationTests {
 
 	@Test
