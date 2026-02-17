3a48bb645235579dbe27494222bf577c7dc87cc4
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-17 10:32:51 +0530

    errors

--- backend/services/recovery-plan-service/src/test/java/com/mentalhealth/recoveryplan/RecoveryplanmicroserviceApplicationTests.java
+++ backend/services/recovery-plan-service/src/test/java/com/mentalhealth/recoveryplan/RecoveryplanmicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.recoveryplanmicroservice.recoveryplan;
+package com.mentalhealth.recoveryplan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
