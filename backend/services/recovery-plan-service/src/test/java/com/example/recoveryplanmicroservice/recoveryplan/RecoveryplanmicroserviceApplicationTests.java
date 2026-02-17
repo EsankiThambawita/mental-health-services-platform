69b9740ed8b78dae0fa273e2b6a9dc53ee2a2ce0
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-17 09:58:43 +0530

    recovery plan initial setup

---
+++ backend/services/recovery-plan-service/src/test/java/com/example/recoveryplanmicroservice/recoveryplan/RecoveryplanmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recoveryplanmicroservice.recoveryplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoveryplanmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
