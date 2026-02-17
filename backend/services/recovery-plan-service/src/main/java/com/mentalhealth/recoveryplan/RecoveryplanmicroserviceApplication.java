1784ac76b9609259a9a49beae3b9053ccb173822
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-02-17 10:19:05 +0530

    solving errors

--- backend/services/recovery-plan-service/src/main/java/com/mentalhealth/recoveryplan/RecoveryplanmicroserviceApplication.java
+++ backend/services/recovery-plan-service/src/main/java/com/mentalhealth/recoveryplan/RecoveryplanmicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.recoveryplanmicroservice.recoveryplan;
+package com.mentalhealth.recoveryplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
