2507304a9ce5eacf7bcf7ac2e75e2e6a7e365bfb
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2026-03-03 10:47:18 +0530

    initial setup

---
+++ backend/services/user-auth-service/src/main/java/com/university/auth/UserAuthServiceApplication.java
@@ -0,0 +1,5 @@
+package com.university.auth;
+
+public class UserAuthServiceApplication {
+
+}
---
+++ backend/services/user-auth-service/src/main/java/com/university/auth/model/User.java
@@ -0,0 +1,8 @@
+package com.university.auth.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+public class User {
+
+}
